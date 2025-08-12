07573741e4e74a237916efc028560e2e9a7fe205
ychoy000 <>
2025-08-12 19:41:30 +0900

    0812

--- cra/my-router/src/App.js
+++ cra/my-router/src/App.js
@@ -1,3 +1,4 @@
+import { HashRouter, Route, Routes, NavLink, useParams } from 'react-router-dom';
 
 function Home(){
   return (
@@ -7,11 +8,49 @@ function Home(){
     </div>
   );
 }
+
+const contents= [
+  {id:1, title:'HTML', description: 'HTML is ...'},
+  {id:2, title:'JS', description: 'JS is ...'},
+  {id:3, title:'REACT', description: 'REACT is ...'},
+]
+function Topic() {
+  const params = useParams();
+  let topic_id = params.topic_id;
+  let selected_topic = {
+    title: 'Sorry',
+    description: 'Not Found'
+  };
+  for(let i=0; i<contents.length; i++) {
+    if(contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    } 
+  }
+  console.log(params);
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  );
+}
+
 function Topics(){
+  const lis =[];
+  for (let i=0; i<contents.length; i++) {
+    lis.push(<li key={contents[i].id}><NavLink to={"/topics/" +contents[i].id}>{contents[i].title}
+    </NavLink></li>);
+  }
   return (
     <div>
       <h2>Topics</h2>
-      Topic...
+      <ul>
+        {lis}
+      </ul>
+      <Routes>
+        <Route path="/:topic_id" element={<Topic />} />
+      </Routes>
     </div>
   );
 }
@@ -26,11 +65,24 @@ function Contact(){
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h2>Hello! Dear</h2>
-      <Home></Home>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/topics/*">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink to="/abcd">ABCD</NavLink></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/topics/*" element={<Topics />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={'Not Found'} />
+      </Routes>
+
+      {/* <Home></Home>
       <Topics></Topics>
-      <Contact></Contact>
+      <Contact></Contact> */}
     </div>
   );
 }
