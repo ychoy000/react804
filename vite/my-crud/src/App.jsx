95a0bcb3d12d6b3d0680f03680e912df41151719
ychoy000 <>
2025-08-20 18:42:32 +0900

    0820

--- vite/my-crud/src/App.jsx
+++ vite/my-crud/src/App.jsx
@@ -63,15 +63,40 @@ function Create(props) {
     </article>
   )
 }
+// Update component
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onUpdate(title, body);
+      }}>
+       <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }} /></p>
+        <p><textarea name="body" placeholder="body" value={body} onChange={event=>{
+          setBody(event.target.value);
+        }}></textarea></p>
+        <p><input type="submit" value="Update"></input></p>
+      </form>
+    </article>
+  )
+}
 
 // App 컴포넌트
 function App() {
-  //const _mode = useState('WEL');
+  //const _mode = useState('WELCOME');
   //console.log('_mode', _mode);
   // const [변수, 함수] = useState(초기값);
   // const [변수, set변수] = useState('WELCOME');
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
   const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
@@ -80,28 +105,64 @@ function App() {
   ]);
 
   let content = null;
+  let contextControl = null;
+
   if (mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, Web"></Article>
   } else if (mode === 'READ') {
     let title, body = null;
     for (let i = 0; i < topics.length; i++) {
       // id는 Number(event.target.id)이다.
-      console.log(topics[i].id, id);
       if (topics[i].id === id) {
         title = topics[i].title
         body = topics[i].body
       }
     }
     content = <Article title={title} body={body}></Article>
-
+    contextControl=<>
+    <li><a href={'/update'+id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+    <li><input type="button" value="Delete" onClick={()=>{
+      const newTopics = []
+      for(let i=0; i<topics.length; i++) {
+        if(topics[i].id !== id) {
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode("WELCOME");
+    }} /></li>
+    </>
   } else if(mode === 'CREATE') {
     content = <Create onCreate={(_title, _body)=>{
-      // 다음의 nextId를 인식못하는 오류 발생
       const newTopic = {id:nextId, title:_title, body:_body}
       const newTopics = [...topics]
+      // console.log(nextId, _title, _body);
       newTopics.push(newTopic);
       setTopics(newTopics);
     }}></Create>
+  } else if(mode === "UPDATE") {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title
+        body = topics[i].body
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      console.log(title, body);
+      const newTopics = [...topics];
+      const updatedTopic = {id:id, title:title, body:body}
+      for(let i=0; i<newTopics.length; i++) {
+        if(newTopics[i].id === id) {
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+    }}></Update>
   }
 
   return (
@@ -116,10 +177,15 @@ function App() {
       {/* <Article title="Welcome" body="Hello, Web"></Article> */}
       {/* <Aticle title={title} body={body}></Aticle> */}
       {content}
-      <a href="/create" onClick={event => {
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
+      <ul>
+        <li>
+          <a href="/create" onClick={event => {
+            event.preventDefault();
+            setMode('CREATE');
+          }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
